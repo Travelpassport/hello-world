a4ea5babea141319f189745ee42c8ccc140b0dbe
Lisa Stappert <Lisa@Lisa.local>
2019-08-02 14:37:32 -0700

    Nathan drank all the beers the website told him to.

---
+++ anything.js
@@ -0,0 +1,15 @@
+
+document.addEventListener ('DOMContentLoaded', () => {
+	const heading = document.getElementById('heading')
+	heading.innerHTML = 'Friyay!'
+
+let count = 0
+	const alcohols = () => {
+		count += 1
+		const alcoholscount = document.getElementById('alcoholscount')
+		alcoholscount.innerHTML = count
+	console.log ('BEERS!')	
+	}
+	window.alcohols = alcohols
+
+	})
